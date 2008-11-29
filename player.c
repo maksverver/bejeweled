aea760b2407d6c265ef3b1f75f453d8d3e750bc0
Maks Verver <maksverver@geocities.com>
2008-11-29 18:12:42 +0100

    Implemented time-limited queue implementation. Seems to work pretty good without need for tuning.

--- player.c
+++ player.c
@@ -7,12 +7,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/time.h>   /* gettimeofday() */
 
-int score_per_move = 430;
-
-static int expected_score(const Board *b)
+/* Return the time in microseconds */
+static long long ustime()
 {
-    return b->score + score_per_move*(MOVE_LIMIT - b->moves);
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return 1000000LL*tv.tv_sec + tv.tv_usec;
 }
 
 /* Compares to boards and returns <0, 0, >0 when the first board is better than,
@@ -20,27 +22,66 @@ static int expected_score(const Board *b)
 */
 static int cmp_board(const void *arg1, const void *arg2)
 {
-    return expected_score((const Board*)arg2) -
-           expected_score((const Board*)arg1);
+    const Board *board1 = arg1, *board2 = arg2;
+    /* Order by decreasing scores, or increasing moves if tied. */
+    int d = board2->score - board1->score;
+    if (d == 0) d = board1->moves - board2->moves;
+    return d;
 }
 
-/* TODO: add time limit to search? */
-static int search(Game *game)
+/* Merge nq into pq, freeing extra boards */
+static void merge_board_queues(PriorityQueue *pq, PriorityQueue *nq)
 {
-    PriorityQueue *pq = pq_create(1000, cmp_board);
+    while (!pq_empty(nq))
+    {
+        if (pq_full(pq)) board_free(pq_pop_max(pq));
+        pq_push(pq, pq_pop_min(nq));
+    }
+}
+
+static int search(Game *game, int max_secs)
+{
+    const size_t queue_cap = 1000;
+
+    /* Priority queue for boards currently being processed */
+    PriorityQueue *pq = pq_create(queue_cap, cmp_board);
     assert(pq != NULL);
+
+    /* Queue for boards with moves == move_limit */
+    PriorityQueue *nq = pq_create(queue_cap, cmp_board);
+    assert(nq != NULL);
 
     /* Keep track of best score so far, and corresponding best last move */
     int best_score = 0;
     int best_moves = 0;
     Move *best_move = NULL;
 
-    pq_push(pq, board_clone(game->initial));
+    /* Time limiting */
+    long long time_start = ustime();
+    long long time_limit = 1000000LL*max_secs;    /* seconds -> microseconds */
+    long long next_update = 0;
+    int move_limit = 1;
+    int iterations = 0;
 
-    long long iterations = 0;
-    while (!pq_empty(pq))
+    pq_push(pq, board_clone(game->initial));
+    while (!pq_empty(pq) || !pq_empty(nq))
     {
         ++iterations;
+        long long time_used = ustime() - time_start;
+
+        if (pq_empty(pq))
+        {
+            /* Increase move limit because we need the new boards */
+            ++move_limit;
+            merge_board_queues(pq, nq);
+        }
+        else
+        if (MOVE_LIMIT * time_used / time_limit > move_limit)
+        {
+            /* Increase move limit because time demands we make progress */
+            move_limit = MOVE_LIMIT * time_used / time_limit;
+            merge_board_queues(pq, nq);
+        }
 
         /* Take next best board from the heap */
         Board *board = pq_pop_min(pq);
@@ -55,10 +96,14 @@ static int search(Game *game)
             move_ref(best_move);
         }
 
-        if (iterations%10000 == 0)
+        if (next_update <= time_used)
         {
-            printf( "iterations=%10lldk score=%10d moves=%10d pq_size=%10d\n",
-                    iterations/1000, board->score, board->moves, pq_size(pq) );
+            printf(
+                "iterations=%10d score=%10d moves=%5d pq_size=%5d nq_size=%5d "
+                "move_limit=%6d est. score: %.3f\n",
+                iterations, board->score, board->moves, pq_size(pq), pq_size(nq),
+                move_limit, 1e-6*best_score/best_moves*MOVE_LIMIT);
+            next_update += 1000000; /* 1 sec */
         }
 
         if (board->moves >= MOVE_LIMIT || board->score >= SCORE_LIMIT)
@@ -82,9 +127,18 @@ static int search(Game *game)
                     assert(new_board != NULL);
                     board_move(new_board, r, c, r + v, c + !v, 1);
 
-                    /* Add to queue if necessary */
-                    if (pq_full(pq)) board_free(pq_pop_max(pq));
-                    pq_push(pq, new_board);
+                    if (new_board->moves < move_limit)
+                    {
+                        /* Add to active queue */
+                        if (pq_full(pq)) board_free(pq_pop_max(pq));
+                        pq_push(pq, new_board);
+                    }
+                    else
+                    {
+                        /* Add to next queue */
+                        if (pq_full(nq)) board_free(pq_pop_max(nq));
+                        pq_push(nq, new_board);
+                    }
                 }
             }
         }
@@ -136,11 +190,7 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
-    int pass;
-    for (pass = 0; pass < 1; ++pass)
-    {
-        score_per_move = search(game);
-    }
+    search(game, 15*60);
 
     game_free(game);
 
